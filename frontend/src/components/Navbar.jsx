0f9da188e410ccd4f54b6627b0b0ad3fc7a7a8f6
Aditya Kulkarni <vip.aditya.k@gmail.com>
2026-03-17 18:36:08 +0530

    favicon add

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -8,7 +8,7 @@ export default function Navbar() {
       <div className="mx-auto flex h-16 max-w-7xl items-center justify-between px-6">
         <div className="flex items-center gap-3">
           <span className="text-xl font-bold tracking-tight text-white">
-            CELER CLIPS
+            <img src="./textlogo.png" width="200"/>
           </span>
         </div>
         
